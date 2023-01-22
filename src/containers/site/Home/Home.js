aa8298858f044e1111ac11ad91f280c2ef56f26d
Enzo.C <Enzocapi@hotmail.com>
2023-01-22 11:59:58 +0100

    Display and design finished on home page

--- src/containers/site/Home/Home.js
+++ src/containers/site/Home/Home.js
@@ -26,22 +26,21 @@ class Home extends Component {
                     </div>
                 </div>
                 <div className='home-content d-flex justify-content-center'>
-                    <div className='card-container d-flex flex-wrap justify-content-center'>
+                    <div className='card-container row d-flex flex-wrap justify-content-around p-5'>
                         {
                             Object.values(this.state.gallerys).filter(item => item.gallery_img && item.gallery_img !== '').map((item, index) => {
                                 return (
-                                    <div className={`card d-flex ${index % 2 === 0 ? "flex-row-reverse" : ""}`}>
-                                        <div className='card-img-container m-4'>
-                                            <img src={item.gallery_img} className='card-img'></img>
+                                    <div className={` row col-12 col-xl-10 mt-5 ${index % 2 === 0 ? "flex-row-reverse" : ""}`}>
+                                        <div className='card-img-container col-8 col-md-6 mx-auto'>
+                                   
+                                            <div className='border-img'>
+                                                <img src={item.gallery_img} className='card-img'></img>
+                                            </div>
+                                        
                                         </div>
-                                        <div className='card-desc m-4'>
-                                            <h2 className=''>{item.gallery_title}</h2>
-
-
-                                            <p className=''>
-                                                Copy code
-                                                {item.gallery_content}
-                                            </p>
+                                        <div className='card-desc col-8 col-md-6 mx-auto mt-md-5'>
+                                            <h2 className={index % 2 === 0 ? "text-end h2Home" : "text-start h2Home"}>{item.gallery_title}</h2>
+                                            <p className={index % 2 === 0 ? "text-end" : "text-start "}>{item.gallery_content}</p>
                                         </div>
                                     </div>
                                 )
