849b08c92a41d21548647366c77c62f00797fe9a
Enzo.C <Enzocapi@hotmail.com>
2023-03-16 00:18:23 +0100

    center footer/changed URL API

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
-import insta from "../../assets/images/insta.png"
-import FB from "../../assets/images/FB.png"
-import './footer.css'
-import HoursTab from '../HoursTab/HoursTab'
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import insta from "../../assets/images/insta.png";
+import FB from "../../assets/images/FB.png";
+import './footer.css';
+import HoursTab from '../HoursTab/HoursTab';
+import { hostname } from '../../config';
 
 const Footer = () => {
 
@@ -11,47 +12,47 @@ const Footer = () => {
 
 
     useEffect(() => {
-        fetch('http://localhost/SERVEURQUAI/front/companyInfo')
+        fetch(`${hostname}front/companyInfo`)
             .then(response => response.json())
             .then(data => setCompanyInfo(data));
     }, []);
 
     return (
-            <footer className='bg-dark'>
-                <div className='row p-3 d-flex justify-content-center align-items-center'>
-                    <div className='col-6 col-md-3 p-3'>
+        <footer className='bg-dark'>
+            <div className='row p-3 d-flex justify-content-center align-items-center text-center'>
+                <div className='col-6 col-md-3 p-3'>
                     <a href='https://www.facebook.com' target="_blank" rel="noopener noreferrer">
-                            <img src={FB} alt='facebook' className='socials' />
-                        </a>
-                    </div>
-                    <div className='col-6 col-md-3 p-3'>
-                    <a href='https://www.instagram.com' target="_blank" rel="noopener noreferrer">
-                            <img src={insta} alt='instagram' className='socials' />
-                        </a>
-                    </div>
-                    <div className='col-6 col-md-3 p-3'>
-                        <NavLink to='/Contact' className={(nav) => (nav.isActive ? "nav-link fw-bold d-inline" : "nav-link fw-bold d-inline")}>
-                            Contact
-                        </NavLink>
-                    </div>
-                    <div className='col-6 col-md-3 p-3'>
-                        <NavLink to='/RGPD' className={(nav) => (nav.isActive ? "nav-link fw-bold d-inline" : "nav-link fw-bold d-inline")}>
-                            Politique de confidentialité
-                        </NavLink>
-                    </div>
-                    <div className='col-12 col-md-6 p-3'>
-                        <HoursTab />
-                    </div>
-                    <div className='col-12 col-md-6 p-3'>
-                        <h1>{companyInfo[1]?.name}</h1>
-                        <h5>{companyInfo[1]?.adress}</h5>
-                        <h5>{companyInfo[1]?.phone}</h5>
-                        <h5>{companyInfo[1]?.email}</h5>
-
-                    </div>
+                        <img src={FB} alt='facebook' className='socials' />
+                    </a>
                 </div>
-                <p className='text-center'>Le Quai Antique - Par Enzo Capitanio - Tout droits réservés</p>
-            </footer>
+                <div className='col-6 col-md-3 p-3'>
+                    <a href='https://www.instagram.com' target="_blank" rel="noopener noreferrer">
+                        <img src={insta} alt='instagram' className='socials' />
+                    </a>
+                </div>
+                <div className='col-6 col-md-3 p-3'>
+                    <NavLink to='/Contact' className={(nav) => (nav.isActive ? "nav-link fw-bold d-inline" : "nav-link fw-bold d-inline")}>
+                        Contact
+                    </NavLink>
+                </div>
+                <div className='col-6 col-md-3 p-3'>
+                    <NavLink to='/RGPD' className={(nav) => (nav.isActive ? "nav-link fw-bold d-inline" : "nav-link fw-bold d-inline")}>
+                        Politique de confidentialité
+                    </NavLink>
+                </div>
+                <div className='col-12 col-md-6 p-3 text-start '>
+                    <HoursTab />
+                </div>
+                <div className='col-12 col-md-6 p-3 text-center'>
+                    <h1>{companyInfo[1]?.name}</h1>
+                    <h5>{companyInfo[1]?.adress}</h5>
+                    <h5>{companyInfo[1]?.phone}</h5>
+                    <h5>{companyInfo[1]?.email}</h5>
+
+                </div>
+            </div>
+            <p className='text-center'>Le Quai Antique - Par Enzo Capitanio - Tout droits réservés</p>
+        </footer>
     );
 }
 
