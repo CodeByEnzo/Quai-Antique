d16974d89ac8e3396b6147e426325cd7bfe6a86d
Enzo.C <Enzocapi@hotmail.com>
2023-03-24 10:14:59 +0100

    added intro on home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Site from './containers/site/Site';
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function App() {
     const [loading, setLoading] = useState(true);
     const loader = document.getElementById("pre-loader");
-    if (loader) {
-        setTimeout(() => {
-            loader.style.display = "none";
-            setLoading(false);
-        }, 800);
-    }
+
+    useEffect(() => {
+        if (loader) {
+            setTimeout(() => {
+                loader.style.display = "none";
+                setLoading(false);
+            }, 800);
+        }
+    }, [loader]);
+
     return (
-        !loading && (
-            <div className="App">
-                <BrowserRouter>
-                    <Site />
-                </BrowserRouter>
-            </div>
-        )
+        <div className="App">
+            {!loading && (
+                <motion.div
+                    className="content-container"
+                    initial={{ scale: 2, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{ duration: 1, delay: 0.3, ease: "easeOut",  }}
+                >
+                    <BrowserRouter>
+                        <Site />
+                    </BrowserRouter>
+                </motion.div>
+            )}
+        </div>
     );
 }
 
