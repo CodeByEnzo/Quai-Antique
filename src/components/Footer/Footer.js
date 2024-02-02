35e563538eba82f05caf9128d381232668315451
Enzo.C <Enzocapi@hotmail.com>
2024-02-02 22:54:01 +0100

    Fixing padding of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,33 +20,41 @@ const Footer = () => {
     return (
         <footer className='bg-dark'>
             <div className='row d-flex justify-content-center align-items-center text-center'>
-                <div className='col-3 p-5'>
+                <div className='col-5 col-md-3 p-5'>
                     <NavLink to='/Contact' className={"nav-link fw-bold d-inline"}>
+                        <hr />
                         Contact
+                        <hr />
                     </NavLink>
                 </div>
-                <div className='col-3 p-5'>
+                <div className='col-5 col-md-3 p-5'>
                     <NavLink to='/RGPD' className={"nav-link fw-bold d-inline"}>
+                        <hr />
                         Politique de confidentialité
+                        <hr />
                     </NavLink>
                 </div>
-                <div className='col-3 p-5'>
+                <div className='col-5 col-md-3 p-5'>
                     <NavLink to='/Legal' className={"nav-link fw-bold d-inline"}>
+                        <hr/>
                         Mentions légales
+                        <hr />
                     </NavLink>
                 </div>
-                <div className='col-3 p-5'>
+                <div className='col-5 col-md-3 px-5 nav-link'>
+                    <hr />
                     <a href='https://www.facebook.com' target="_blank" rel="noopener noreferrer">
-                        <img src={FB} alt='facebook' className='socials' />
+                        <img src={FB} alt='facebook1' className='socials px-3' />
                     </a>
                     <a href='https://www.instagram.com' target="_blank" rel="noopener noreferrer">
-                        <img src={insta} alt='instagram' className='socials' />
+                        <img src={insta} alt='instagram' className='socials px-3' />
                     </a>
+                    <hr />
                 </div>
-                <div className='col-12 col-md-5 p-3 text-start '>
+                <div className='col-12 col-md-5 p-5 text-start'>
                     <HoursTab />
                 </div>
-                <div className='col-12 col-md-5 p-3 text-center'>
+                <div className='col-12 col-md-5 p-1 text-center'>
                     <h1>{companyInfo[1]?.name}</h1>
                     <h5>{companyInfo[1]?.adress}</h5>
                     <h5>{companyInfo[1]?.phone}</h5>
