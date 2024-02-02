ca12abfdf30ff26664783111ee4121051abeddb9
Enzo.C <Enzocapi@hotmail.com>
2024-02-02 22:42:06 +0100

    Better design of Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,44 +19,43 @@ const Footer = () => {
 
     return (
         <footer className='bg-dark'>
-            <div className='row p-3 d-flex justify-content-center align-items-center text-center'>
-                <div className='col-6 col-md-3 p-3'>
+            <div className='row d-flex justify-content-center align-items-center text-center'>
+                <div className='col-3 p-5'>
+                    <NavLink to='/Contact' className={"nav-link fw-bold d-inline"}>
+                        Contact
+                    </NavLink>
+                </div>
+                <div className='col-3 p-5'>
+                    <NavLink to='/RGPD' className={"nav-link fw-bold d-inline"}>
+                        Politique de confidentialité
+                    </NavLink>
+                </div>
+                <div className='col-3 p-5'>
+                    <NavLink to='/Legal' className={"nav-link fw-bold d-inline"}>
+                        Mentions légales
+                    </NavLink>
+                </div>
+                <div className='col-3 p-5'>
                     <a href='https://www.facebook.com' target="_blank" rel="noopener noreferrer">
                         <img src={FB} alt='facebook' className='socials' />
                     </a>
-                </div>
-                <div className='col-6 col-md-3 p-3'>
                     <a href='https://www.instagram.com' target="_blank" rel="noopener noreferrer">
                         <img src={insta} alt='instagram' className='socials' />
                     </a>
                 </div>
-                <div className='col-6 col-md-3 p-3'>
-                    <NavLink to='/Contact' className={(nav) => (nav.isActive ? "nav-link fw-bold d-inline" : "nav-link fw-bold d-inline")}>
-                        Contact
-                    </NavLink>
-                </div>
-                <div className='col-6 col-md-3 p-3'>
-                    <NavLink to='/RGPD' className={(nav) => (nav.isActive ? "nav-link fw-bold d-inline" : "nav-link fw-bold d-inline")}>
-                        Politique de confidentialité
-                    </NavLink>
-                </div>
-                <div className='col-6 col-md-3 p-3'>
-                    <NavLink to='/Legal' className={(nav) => (nav.isActive ? "nav-link fw-bold d-inline" : "nav-link fw-bold d-inline")}>
-                        Mentions légales
-                    </NavLink>
-                </div>
-                <div className='col-12 col-md-6 p-3 text-start '>
+                <div className='col-12 col-md-5 p3 text-start '>
                     <HoursTab />
                 </div>
-                <div className='col-12 col-md-6 p-3 text-center'>
+                <div className='col-12 col-md-5 p-3 text-center'>
                     <h1>{companyInfo[1]?.name}</h1>
                     <h5>{companyInfo[1]?.adress}</h5>
                     <h5>{companyInfo[1]?.phone}</h5>
                     <h5>{companyInfo[1]?.email}</h5>
-
                 </div>
             </div>
-            <p className='text-center m-0'>Le Quai Antique - Par Enzo Capitanio - Tout droits réservés</p>
+            <p className='text-center m-0'>
+                Le Quai Antique - Par Enzo Capitanio - Tout droits réservés
+            </p>
         </footer>
     );
 }
